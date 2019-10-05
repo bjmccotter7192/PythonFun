707eae269a90114425360dd17ca51616ea582a72
BJ McCotter <bj.mccotter@veteransunited.com>
2019-10-05 18:04:26 -0500

    added a few things to play with

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 import gzip
 import json
-import numpy as np
-import matplotlib.pyplot as plt
+from mnist import MNIST
 
-trainImages = gzip.open('train-images-idx3-ubyte.gz', 'r')
-trainLabels = gzip.open('train-labels-idx1-ubyte.gz', 'r')
+mndata = MNIST('samples')
+mndata.gz = True
 
-image_size = 28
-num_images = 5
+images, labels = mndata.load_training()
+# or
+images, labels = mndata.load_testing()
 
-trainImages.read(16)
-buf = trainImages.read(image_size * image_size * num_images)
-data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-data = data.reshape(num_images, image_size, image_size, 1)
-
-image = np.asarray(data[4]).squeeze()
-plt.imshow(image)
-plt.show()
+index = random.randrange(0, len(images))
+print(mndata.display(images[index]))
---
+++ mnistUsingBuffer.py
@@ -0,0 +1,20 @@
+import gzip
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+trainImages = gzip.open('train-images-idx3-ubyte.gz', 'r')
+trainLabels = gzip.open('train-labels-idx1-ubyte.gz', 'r')
+
+image_size = 28
+num_images = 200
+
+# trainImages.read(16)
+buf = trainImages.read(image_size * image_size * num_images)
+data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+data = data.reshape(num_images, image_size, image_size, 1)
+
+image = np.asarray(data[1]).squeeze()
+plt.imshow(image)
+plt.savefig('testFig.png')
+plt.show()
