77dfec559331da083780d619a6732fe211190327
BJ McCotter <bj.mccotter@veteransunited.com>
2019-10-11 16:01:16 -0700

    temp checkin in

--- app.py
+++ app.py
@@ -1,13 +1,49 @@
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
 import gzip
-import json
-from mnist import MNIST
 
-mndata = MNIST('samples')
-mndata.gz = True
+def read_image(file_name, idx_image):
+#file_name: If used for the MNIST dataset, should be either 
+#train-images-idx3-ubyte or t10k-images-idx3-ubyte
+#index of the image you want to read.
 
-images, labels = mndata.load_training()
-# or
-images, labels = mndata.load_testing()
+    img_file = open(file_name,'r+b')
+    print(img_file)
+##########################################
+# Get basic information about the images #
+# (This is described in the webpage of 	 #
+# the database)							 #
+##########################################
+    img_file.seek(0)
+    magic_number = img_file.read(4)
+    magic_number = struct.unpack('>i',magic_number)
+    print('Magic Number: '+str(magic_number[0]))
+    data_type = img_file.read(4)
+    data_type = struct.unpack('>i',data_type)
+    print('Number of Images: '+str(data_type[0]))
+    dim = img_file.read(8)
+    dimr = struct.unpack('>i',dim[0:4])
+    dimr = dimr[0]
+    print('Number of Rows: '+str(dimr))
+    dimc = struct.unpack('>i',dim[4:])
+    dimc = dimc[0]
+    print('Number of Columns:'+str(dimc))
+    image = np.ndarray(shape=(dimr,dimc))
+    img_file.seek(16+dimc*dimr*idx_image)
+    for row in range(dimr):
+        for col in range(dimc):
+            tmp_d = img_file.read(1)
+            tmp_d = struct.unpack('>B',tmp_d)
+            image[row,col] = tmp_d[0]
+    img_file.close()
+    return image
 
-index = random.randrange(0, len(images))
-print(mndata.display(images[index]))
+image = read_image("train-images.idx3-ubyte",1)
+
+print(image.tolist())
+
+img_plot = plt.imshow(image,'Greys')
+plt.savefig('testFig.png')
+plt.show()
--- mnistUsingBuffer.py
+++ mnistUsingBuffer.py
@@ -9,10 +9,14 @@ trainLabels = gzip.open('train-labels-idx1-ubyte.gz', 'r')
 image_size = 28
 num_images = 200
 
-# trainImages.read(16)
+trainImages.read(16)
 buf = trainImages.read(image_size * image_size * num_images)
 data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
 data = data.reshape(num_images, image_size, image_size, 1)
+# test = data.reshape(748, 1)
+
+print(np.asarray(data[1]))
+print(test)
 
 image = np.asarray(data[1]).squeeze()
 plt.imshow(image)
